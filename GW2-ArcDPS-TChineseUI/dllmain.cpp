4bdcdc6b75df9f0d99a3fe446368feaa7cef399c
Cavey <m.123051427@gmail.com>
2026-06-06 18:48:55 +0800

    feat: 更新 DLL 進入點，新增初始化與釋放模組的功能

--- GW2-ArcDPS-TChineseUI/dllmain.cpp
+++ GW2-ArcDPS-TChineseUI/dllmain.cpp
@@ -1,19 +1,36 @@
-﻿// dllmain.cpp : 定義 DLL 應用程式的進入點。
-#include "pch.h"
+﻿#include <arcdps_structs.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+static arcdps_exports arc_exports = { 0 };
+
+arcdps_exports* init_module() {
+	arc_exports.size = sizeof(arcdps_exports);
+	arc_exports.sig = 0x54434849;
+	arc_exports.imguivers = 19270;
+	arc_exports.out_name = "GW2-ArcDPS-TChineseUI";
+	arc_exports.out_build = "1.0.0";
+
+	return &arc_exports;
 }
 
+void release_module() {
+}
+
+extern "C" __declspec(dllexport) void* get_init_addr(
+    char* arcversionstr,
+    void* imguicontext,
+    void* id3dptr,
+    HANDLE arcdll,
+    void* mallocfn,
+    void* freefn,
+    uint32_t imguiversion)
+{
+    return (void*)&init_module;
+}
+
+extern "C" __declspec(dllexport) void* get_release_addr(uint32_t reason) {
+    return (void*)&release_module;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    return TRUE;
+}
