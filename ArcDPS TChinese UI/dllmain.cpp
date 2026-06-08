a51b8d1d9609f352508c23b92bbd4714e341270e
Cavey <m.123051427@gmail.com>
2026-06-09 00:53:32 +0800

    feat: 增強模組初始化，加入錯誤處理及選項標籤功能

--- ArcDPS TChinese UI/dllmain.cpp
+++ ArcDPS TChinese UI/dllmain.cpp
@@ -1,5 +1,9 @@
-﻿#include <imgui/imgui.h>
+﻿#include "TabUI.h"
+#include "GW2LangPatch.h" 
 
+#include <stdexcept>
+#include <string>
+#include <imgui/imgui.h>
 #include <arcdps_structs.h>
 
 /* proto/globals */
@@ -8,22 +12,71 @@ extern "C" __declspec(dllexport) void* get_init_addr(char* arcversionstr, void* 
 extern "C" __declspec(dllexport) void* get_release_addr(uint32_t reason);
 arcdps_exports* init_module();
 void release_module();
+void options_tab();
+void imgui_callback(uint32_t not_charsel_or_loading, uint32_t hide_if_combat_or_ooc);
+
+HMODULE arc_dll;
 
 // get exports
 e3_func_ptr arc_log_file;
 e3_func_ptr arc_log;
 
-arcdps_exports* init_module() {
-	arc_exports.size = sizeof(arcdps_exports);
-	arc_exports.sig = 0x54434849;
-    arc_exports.imguivers = IMGUI_VERSION_NUM;
-	arc_exports.out_name = "TChinese UI";
-	arc_exports.out_build = "0.0.1";
+void ArcLog(const char* message) {
+    if (arc_log_file) arc_log_file(const_cast<char*>(message));
+}
 
-	return &arc_exports;
+arcdps_exports* init_module() {
+
+    bool loading_successful = true;
+    std::string error_message = "Unknown error";
+
+    try {
+        if (!GW2LangPatch::Initialize()) {
+            throw std::runtime_error("GW2LangPatch initialization failed. Memory signatures not found.");
+        }
+    }
+    catch (std::exception& e)
+    {
+        loading_successful = false;
+        error_message = "Error starting up: ";
+        error_message.append(e.what());
+        ArcLog(error_message.c_str());
+    }
+
+    arc_exports.imguivers = IMGUI_VERSION_NUM;
+    arc_exports.out_name = "TChinese UI";
+    arc_exports.out_build = "0.0.1";
+
+    if (loading_successful)
+    {
+        arc_exports.size = sizeof(arcdps_exports);
+        arc_exports.sig = 0x54434849;
+        arc_exports.options_end = options_tab;
+        arc_exports.imgui = imgui_callback;
+    }
+    else
+    {
+        arc_exports.sig = 0;
+        const std::string::size_type size = error_message.size();
+        char* buffer = new char[size + 1]; //we need extra char for NUL
+        memcpy(buffer, error_message.c_str(), size + 1);
+        arc_exports.size = (uintptr_t)buffer;
+    }
+
+    return &arc_exports;
 }
 
 void release_module() {
+    GW2LangPatch::Shutdown();
+}
+
+void options_tab()
+{
+    tabui.Draw();
+}
+
+void imgui_callback(uint32_t not_charsel_or_loading, uint32_t hide_if_combat_or_ooc) {
+    GW2LangPatch::Update();
 }
 
 extern "C" __declspec(dllexport) void* get_init_addr(
@@ -36,8 +89,9 @@ extern "C" __declspec(dllexport) void* get_init_addr(
     uint32_t imguiversion)
 {
     // set all arcdps stuff
-    arc_log_file = (arc_log_func_ptr)GetProcAddress(arc_dll, "e3");
-    arc_log = (arc_log_func_ptr)GetProcAddress(arc_dll, "e8");
+    arc_dll = (HMODULE)arcdll;
+    arc_log_file = (e3_func_ptr)GetProcAddress(arc_dll, "e3");
+    arc_log = (e3_func_ptr)GetProcAddress(arc_dll, "e8");
 
     // set imgui context && allocation for arcdps dll space
     ImGui::SetCurrentContext(static_cast<ImGuiContext*>(imguicontext));
